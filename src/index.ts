4a66d2ae13619a1bf312fa22cb059bfe92d3eeb4
Fahad <fahad.45854@iqra.edu.com>
2024-07-29 23:29:55 +0500

    code shifted

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,31 @@
-// url for this server: https://book-review-platform-server.vercel.app
+// // url for this server: https://book-review-platform-server.vercel.app
+
+
 
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import cookieParser from 'cookie-parser';
 
-import Routes from './routes'
-
-import {connectDB} from './config/db'
+import Routes from './routes';
+import { connectDB } from './config/db';
 
 dotenv.config();
 
 const app: Express = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.json());
-// app.use(cors({
-//     // origin: 'http://localhost:5173', // Replace with your front-end URL
-//     origin: 'https://book-review-platform-gold.vercel.app', // Replace with your front-end URL
-//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-//     credentials: true,
-// }));
-app.options('*',cors({
-    // origin: 'http://localhost:5173', // Replace with your front-end URL
+// Use CORS middleware before other middleware and routes
+app.use(cors({
     origin: 'https://book-review-platform-gold.vercel.app', // Replace with your front-end URL
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
 }));
+
+app.use(express.json());
 app.use(cookieParser());
 
-connectDB()
+connectDB();
 
 app.use("/api", Routes);
 
@@ -40,3 +36,51 @@ app.get("/", (req: Request, res: Response) => {
 app.listen(PORT, () => {
     console.log(`[server]: Server is running at http://localhost:${PORT}`);
 });
+
+
+
+
+
+
+
+
+// import express, { Express, Request, Response } from "express";
+// import dotenv from "dotenv";
+// import cors from "cors";
+// import cookieParser from 'cookie-parser';
+
+// import Routes from './routes'
+
+// import {connectDB} from './config/db'
+
+// dotenv.config();
+
+// const app: Express = express();
+// const PORT = process.env.PORT || 3000;
+
+// app.use(express.json());
+// // app.use(cors({
+// //     // origin: 'http://localhost:5173', // Replace with your front-end URL
+// //     origin: 'https://book-review-platform-gold.vercel.app', // Replace with your front-end URL
+// //     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+// //     credentials: true,
+// // }));
+// app.options('*',cors({
+//     // origin: 'http://localhost:5173', // Replace with your front-end URL
+//     origin: 'https://book-review-platform-gold.vercel.app', // Replace with your front-end URL
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//     credentials: true,
+// }));
+// app.use(cookieParser());
+
+// connectDB()
+
+// app.use("/api", Routes);
+
+// app.get("/", (req: Request, res: Response) => {
+//     res.send("Express + TypeScript Server");
+// });
+
+// app.listen(PORT, () => {
+//     console.log(`[server]: Server is running at http://localhost:${PORT}`);
+// });
