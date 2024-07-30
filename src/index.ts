4a39dea6cefb6b698a6c71f1c093b9b8c3d042ff
Fahad Abdul Qayyum <100997736+FahadAbdulQayyum@users.noreply.github.com>
2024-07-30 06:13:52 +0500

    Update index.ts
    URL replaced for cors

--- src/index.ts
+++ src/index.ts
@@ -17,8 +17,8 @@ const PORT = process.env.PORT || 3000;
 
 // Use CORS middleware before other middleware and routes
 app.use(cors({
-    // origin: 'https://book-review-platform-gold.vercel.app', // Replace with your front-end URL
-    origin: 'http://localhost:5173', // Replace with your front-end URL
+    origin: 'https://book-review-platform-gold.vercel.app', // Replace with your front-end URL
+    // origin: 'http://localhost:5173', // Replace with your front-end URL
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
 }));
